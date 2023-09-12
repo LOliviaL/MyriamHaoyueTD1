c5ed89466c4a9ec29833e41d3ab655c560652d22
LOliviaL <liuhaoyue1024@gmail.com>
2023-09-12 15:32:52 +0200

    header footer

---
+++ src/app/carre/carre.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carre',
+  templateUrl: './carre.component.html',
+  styleUrls: ['./carre.component.scss']
+})
+export class CarreComponent {
+
+}
